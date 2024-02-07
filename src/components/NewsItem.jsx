544360701da5fcb55412d494590c28f4166e7a3b
QTsom <somin5998@gmail.com>
2024-02-07 15:25:19 +0900

    feat: connection news api

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,14 +1,34 @@
 import styled from "styled-components"
 
-const NewsItem = () => {
+const NewsItem = ({article}) => {
+
+    const { title, description, url, urlToImage} = article;
+
     return (
         <NewsItemContainer>
+            {urlToImage && (
+                <div>
+                    <a href={url}>
+                        <img src={urlToImage} alt="" />
+                    </a>
+                </div>
+            )}
 
+            <div className="contents">
+                <h2>
+                    <a href={url}>
+                        {title}
+                    </a>
+                </h2>
+                <p>
+                    {description}
+                </p>
+            </div>
         </NewsItemContainer>
     )
 }
 
-const NewsItemContainer = styled.li`
+const NewsItemContainer = styled.article`
     
 `
 
--- src/pages/Menu2.jsx
+++ src/pages/Menu2.jsx
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react";
 import Layout from "components/common/Layout";
 import styled from "styled-components";
 import NewsItem from "components/NewsItem";
+import axios from 'axios';
 
-const Menu2 = () => {
+const Menu2 = ({category}) => {
+
+    const [articles, setArticles] = useState();
+    const [loading, setLoading] = useState();
+    const query = category === "all" ? "" : `&category=${category}`;
+
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true);
+
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d6e76706631d4059b20e981f9fc9b930'
+                );
+
+                setArticles(response.data.articles);
+            } catch(error) {
+                console.log(error)
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [category])
+
+    if (loading) {
+        return (
+            <Layout>
+                <Menu2Container>
+                    <div className="inner">
+                        <div className="title-wrap">
+                            <h2 className="content-title">News Api</h2>
+                            <p className="title-wrap--description">외부 news api를 호출해 뉴스뷰어 작업</p>
+                        </div>
+
+                        <p className="">대기 중</p>
+                    </div>
+                </Menu2Container>
+            </Layout>
+        )
+    }
+    if (!articles) {
+        return null;
+    }
+
     return (
         <Layout>
             <Menu2Container>
@@ -13,7 +58,9 @@ const Menu2 = () => {
                     </div>
 
                     <ul>
-                        <NewsItem />
+                        {articles.map((article) => (
+                            <NewsItem key={article.url} article={article} />
+                        ))}
                     </ul>
                 </div>
             </Menu2Container>
