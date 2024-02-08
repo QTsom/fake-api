1b54b2e81cc830e05e6c950739f8e7e32c028997
QTsom <somin5998@gmail.com>
2024-02-08 11:45:49 +0900

    feat: category component

--- src/components/NewsCategory.jsx
+++ src/components/NewsCategory.jsx
@@ -1,9 +1,43 @@
 import styled from "styled-components";
 
+const categories = [
+    {
+        name: 'all',
+        text: '전체보기',
+    },
+    {
+        name: 'business',
+        text: '비즈니스',
+    },
+    {
+        name: 'entertainment',
+        text: '엔터테이먼트',
+    },
+    {
+        name: 'health',
+        text: '건강',
+    },
+    {
+        name: 'science',
+        text: '과학',
+    },
+    {
+        name: 'sports',
+        text: '스포츠',
+    },
+    {
+        name: 'technology',
+        text: '기술',
+    },
+]
 const NewsCategory = () => {
     return (
         <NewsCategoryContainer>
-            
+            {categories.map(c => (
+                <div key={c.name}>
+                    {c.text}
+                </div>
+            ))}
         </NewsCategoryContainer>
     )
 }
